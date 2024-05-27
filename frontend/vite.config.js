6748259d395a7bf046bf691695e6e7bb4260ada5
bishal <bi.gb.ak.cs.it@gmail.com>
2024-05-27 19:42:19 +0530

    config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server : {
     proxy : {
       '/api' : {
-        target : 'http://localhost:3000',
+        target : "https://rentify-ng9e.vercel.app/",
         secure : false,
       },
     },
